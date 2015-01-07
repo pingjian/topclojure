d9cf91b65d6b81bb26450cc6b5775d24f39f63d5
a <a@example.com>
2015-01-08 07:54:48 +0900

    Creates skelton

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to topclojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/topclojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns topclojure.core-test
+  (:require [clojure.test :refer :all]
+            [topclojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
