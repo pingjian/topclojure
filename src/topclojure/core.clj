e294a4b9395ade73afe7bea4dd43797250b0d69f
a <a@example.com>
2015-01-08 13:25:25 +0900

    Fetches signature

--- src/topclojure/core.clj
+++ src/topclojure/core.clj
@@ -1,7 +1,18 @@
 (ns topclojure.core
-  (:gen-class))
+  (:require [net.cgrand.enlive-html :as html]))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn fetch-signature
+  [url]
+  (html/texts (html/select (fetch-html url) function-selector)))
+
+(def function-selector
+  [:td.statText :> :table :> (html/nth-child 5) :> (html/nth-child 2)])
+
+
+(defn fetch-html
+  [url]
+  (html/html-resource (java.net.URL. url)))
