2ff3752de97e9a63e1ee603ec343bd84e7caf219
a <a@example.com>
2015-01-09 04:27:43 +0900

    Fetches inputs and outputs

--- src/topclojure/core.clj
+++ src/topclojure/core.clj
@@ -10,27 +10,34 @@
   [url]
   (let [function-selector
         [:td.statText :> :table :> (html/nth-child 5) :> (html/nth-child 2)]]
-    (apply html/text (html/select (fetch-html url) function-selector))))
+    (html/texts (html/select (fetch-html url) function-selector))))
 
 (defn retrieve-function
   [signature]
   (let [pattern (re-pattern #"\s([^(]+)\(")
-        matcher (re-matcher pattern signature)]
+        matcher (re-matcher pattern (str signature))]
     (nth (re-find matcher) 1)))
 
 (defn retrieve-parameters
   [signature]
-  (re-seq #"[^\s,)(]+(?=[,)])" signature))
+  (re-seq #"[^\s,)(]+(?=[,)])" (str signature)))
 
 (defn count-parameters
   [signature]
   (count (retrieve-parameters signature)))
+
+(defn fetch-ios
+  [url]
+  (let [io-selector [:pre]
+        parameter-count (count-parameters (fetch-signature url))]
+    (partition parameter-count
+               (map html/text (html/select (fetch-html url) io-selector)))))
 
 (defn -main
   [url]
   (let [function (retrieve-function (fetch-signature url))
         parameters (retrieve-parameters (fetch-signature url))]
     (selmer/render-file "template.clj"
-                        {:function function
-                        :parameters parameters})))
+                        {:function   function
+                         :parameters parameters})))
 
