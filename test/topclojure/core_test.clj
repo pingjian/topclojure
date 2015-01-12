e553aa6b439217c9f6fc997708355ba04e84cb41
a <a@example.com>
2015-01-12 10:16:39 +0900

    Uses :require :as instead of :require :refer :all

--- test/topclojure/core_test.clj
+++ test/topclojure/core_test.clj
@@ -1,19 +1,19 @@
 (ns topclojure.core-test
-  (:require [clojure.test :refer :all]
-            [topclojure.core :refer :all]))
+  (:require [clojure.test :as test]
+            [topclojure.core :as tc]))
 
-(deftest test-retrieve-parameters
-  (are [x y] (= x y)
-             '("attributes") (retrieve-parameters "int train(int[] attributes)")
-             '("K" "danceCost") (retrieve-parameters
+(test/deftest test-retrieve-parameters
+  (test/are [x y] (= x y)
+             '("attributes") (tc/retrieve-parameters "int train(int[] attributes)")
+             '("K" "danceCost") (tc/retrieve-parameters
                                   "int minimum(int K, int[] danceCost)")))
 
-(deftest test-retrieve-ios
-  (are [x y] (= x y)
-             "A" (retrieve-ios "A")
-             "[]" (retrieve-ios "{}")))
+(test/deftest test-retrieve-ios
+  (test/are [x y] (= x y)
+             "A" (tc/retrieve-ios "A")
+             "[]" (tc/retrieve-ios "{}")))
 
-(deftest test-pack-ios
-  (are [x y] (= x y)
-             [["[1 2 3]" "3"] ["[5 5]" "0"]] (pack-ios '("[1 2 3]" "3" "[5 5]" "0") 1)))
+(test/deftest test-pack-ios
+  (test/are [x y] (= x y)
+             [["[1 2 3]" "3"] ["[5 5]" "0"]] (tc/pack-ios '("[1 2 3]" "3" "[5 5]" "0") 1)))
 
