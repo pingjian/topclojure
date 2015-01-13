33843d5cf89aff7a6d179ee82c6fc6683c8677e1
a <a@example.com>
2015-01-13 10:46:42 +0900

    Adds numeric-tower and conmbinatorics

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/math.combinatorics "0.0.8"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
                  [enlive "1.1.5"]
                  [selmer "0.7.9"]]
   :main ^:skip-aot topclojure.core
